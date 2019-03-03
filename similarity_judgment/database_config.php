4076ed36018f4486f8cd43769ed65b3e6bf76837
wolfdewitte <47540462+wolfdewitte@users.noreply.github.com>
2019-03-03 15:06:00 +0100

    Add files via upload

---
+++ similarity_judgment/database_config.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = "path_for_your_sql_server";
+$port = 3306; //usually 3306, but might differ
+$username = "yours_sql_username"; //if several people use your database, these would differ
+$password = "your_sql_password"; //if several people use your database, these would differ
+$dbname = "name_of_your_database"; //this has all your tables in it
+$table = "your_experiment_table_in_your_database"; //this differs for each experiment
+
+?>
